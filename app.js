b23d60d1051e04a7be599c90572cd3c89ccee3ff
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2022-05-22 14:18:37 +0100

    Use .bin/www

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
-const exp = require("constants");
 
 var app = express();
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'views'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 app.get("/", (req, res) =>
 {
     res.render("index");
 });
 
-
-
-app.listen(3000, () => { console.log("Server started on Port 3000..."); })
+module.exports = app;
