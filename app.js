b902dac7e2eb914cb52e5b5084b543e9075fe792
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2022-05-26 13:47:40 +0100

    Make semi-decent start

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-var app = express();
+let app = express();
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'views'));
--- public/js/button.js
+++
@@ -1,7 +0,0 @@
-var count = 0;
-function onButtonPressed() {
-    var b = document.getElementById("myButton");
-    count = count + 1;
-    b.innerHTML = "Clicked " + count.toString() + " Times";
-    console.log("clicked button " + count.toString() + " Times");
-}
